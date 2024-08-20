e51e857287fb4c34f14502f6f3b32c651f24f049
Eltun Məmmədli <meltun4255540@gmail.com>
2024-08-20 17:33:48 +0400

    Task 24

---
+++ Task_24/index.js
@@ -0,0 +1,39 @@
+
+document.getElementsByClassName("Button")[0].addEventListener("click", function() {
+   
+    document.getElementsByClassName("cards")[0].style.cssText = "display : block; margin-top : 20px; " ;
+
+
+    let paragraf = document.getElementsByClassName("head")[0];
+    
+
+    let nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.placeholder = "Konteynerə ad verin...";
+    nameInput.style.cssText = "margin-top: 10px; margin-bottom: 10px; width : 100%; padding-top : 10px; border : 0px solid white";
+
+  
+    paragraf.replaceWith(nameInput);
+
+    document.getElementsByClassName("Button_2")[0].addEventListener("click", function(){
+        document.getElementsByClassName("text")[0].style.cssText = "display : block; margin-top : 10px" ;
+
+         
+   
+        let button = document.getElementsByClassName("Button_2")[0];
+
+        
+        document.getElementsByClassName("cards")[0].appendChild(button);
+    })
+
+ 
+   
+    let button = document.getElementsByClassName("Button")[0];
+
+    
+    document.getElementsByClassName("main_container")[0].appendChild(button);
+
+
+
+});
+
