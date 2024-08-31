14209fbbefac8e2fe64f49ac2a9dbf756e30736c
Eltun Məmmədli <meltun4255540@gmail.com>
2024-08-31 22:31:18 +0400

    Task 24

--- Task_24/index.js
+++ Task_24/index.js
@@ -1,37 +1,42 @@
-document.getElementsByClassName("Button")[0].addEventListener("click", function() {
-   
-    document.getElementsByClassName("cards")[0].style.cssText = "display : block; margin-top : 20px; " ;
+let Card = `
+<li class="cards">
+    <div class="div">
+        <input type="text" class="head" placeholder="Konteyner adını daxil edin:">
+    </div>
+    <button class="Button_2">
+        <p>+</p>
+    </button>
+</li>`;
 
+document.getElementsByClassName("Button")[0].addEventListener("click", function(){
 
-    let paragraf = document.getElementsByClassName("head")[0];
+    let container = document.getElementsByClassName("main_container")[0];
     
 
-    let nameInput = document.createElement("input");
-    nameInput.type = "text";
-    nameInput.placeholder = "Konteynerə ad verin...";
-    nameInput.style.cssText = "margin-top: 10px; margin-bottom: 10px; width : 100%; padding-top : 10px; border : 0px solid white";
+    container.insertAdjacentHTML('beforeend', Card);
 
-  
-    paragraf.replaceWith(nameInput);
 
-    document.getElementsByClassName("Button_2")[0].addEventListener("click", function(){
-        document.getElementsByClassName("text")[0].style.cssText = "display : block; margin-top : 10px" ;
-
-         
-   
-        let button = document.getElementsByClassName("Button_2")[0];
-
-        
-        document.getElementsByClassName("cards")[0].appendChild(button);
-    })
-
- 
-   
-    let button = document.getElementsByClassName("Button")[0];
+    let cards = document.getElementsByClassName("cards");
+    let lastCard = cards[cards.length - 1];
 
     
-    document.getElementsByClassName("main_container")[0].appendChild(button);
-
-
-
+    lastCard.querySelector(".Button_2").addEventListener("click", function(){
+        lastCard.insertAdjacentHTML('beforeend', Properties);
+    });
 });
+
+let Properties = `
+<div class="text">
+    <p class="description">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    <div class="img">
+        <img src="justify.png" alt="" style="margin-right: 15px;">
+        <img src="checkbox.png" alt="">
+        <p>4/8</p>
+        <img src="attach.png" alt="">
+        <p>2</p>
+        <img src="comments.png" alt="">
+        <p style="margin: 0 20px 0 5px;">4</p>
+        <img src="man_1.png" alt="" style="width: 40px; height: 40px; margin-top: -10px;">
+    </div>
+</div>`;
